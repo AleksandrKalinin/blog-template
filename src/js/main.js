9d4d4746423b885de1f4e5fb9d4e37862f38e4bc
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2021-09-15 10:40:16 +0300

    minor interactivity fixes

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,23 @@ for (var i = 0; i < links.length; i++) {
     bottomBar.classList.toggle('bottom-bar_rotated');    
   })
 }
+
+var tabs = document.getElementsByClassName('sidebar-tabs__item');
+
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', function(e) {    
+    for (var j = 0; j < tabs.length; j++) {
+      tabs[j].classList.remove('tabs-item_active');
+    }
+    e.target.classList.add('tabs-item_active');   
+  })
+}
+
+
+var search = document.getElementById('search');
+var searhIcon = document.getElementById('searchIcon'); 
+
+search.addEventListener('change', function() {
+  console.log("console");
+  searchIcon.style.display = 'none';
+})
