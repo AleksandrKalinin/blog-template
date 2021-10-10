1aea9b9c1b94913567c1da458bc353eba41f1503
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2021-10-11 00:27:25 +0300

    event listener on parent, named function as a parameter in event listener

--- src/js/main.js
+++ src/js/main.js
@@ -4,37 +4,27 @@ var overlay = document.getElementById('menuOverlay');
 var topBar = document.getElementById('topBar');
 var middleBar = document.getElementById('middleBar');
 var bottomBar = document.getElementById('bottomBar');
+var tabsWrap = document.getElementById('tabsWrap');
 
-menu.addEventListener('click', function(){
+var toggleMenu = function() {
   body.classList.toggle('stop-scrolling');
   menu.classList.toggle('menu_open');
   overlay.classList.toggle('overlay_open');
   topBar.classList.toggle('top-bar_rotated');
   middleBar.classList.toggle('middle-bar_rotated');
-  bottomBar.classList.toggle('bottom-bar_rotated');
-})
-
-var links = document.getElementsByClassName('menu-link');
-
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', function(){    
-    body.classList.toggle('stop-scrolling');
-    menu.classList.toggle('menu_open');
-    overlay.classList.toggle('overlay_open');
-    topBar.classList.toggle('top-bar_rotated');
-    middleBar.classList.toggle('middle-bar_rotated');
-    bottomBar.classList.toggle('bottom-bar_rotated');    
-  })
+  bottomBar.classList.toggle('bottom-bar_rotated');	
 }
+
+menu.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu)
 
 var tabs = document.getElementsByClassName('sidebar-tabs__item');
 
-for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener('click', function(e) {    
-    for (var j = 0; j < tabs.length; j++) {
-      tabs[j].classList.remove('tabs-item_active');
-    }
-    e.target.classList.add('tabs-item_active');   
-  })
-}
-
+tabsWrap.addEventListener('click', function(event) {
+	if (event.target.classList.contains('sidebar-tabs__item')) {
+		for (var i = 0; i < tabs.length; i++) {
+			tabs[i].classList.remove('tabs-item_active');
+		}
+		event.target.classList.add('tabs-item_active');
+	}
+});
